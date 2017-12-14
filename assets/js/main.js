b3027739ada3e4f8b89b69c09a8f87280465d4b6
Vera O. Michael <heavenlyvera90@gmail.com>
2017-12-14 10:59:14 +0100

    wristwatch

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Vera Michael on 5/22/2017.
+ */
+
+$(function(){
+
+var header = $(".slide").owlCarousel({
+    items: 1,
+    dots: false,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 1000
+});
+    
+    
+})
